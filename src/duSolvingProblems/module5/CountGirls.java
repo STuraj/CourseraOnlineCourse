8f61bfede83cafd23db65dd6b35b6ac546e30d6b
Turaj <h_turaj@yahoo.com>
2026-04-26 15:44:45 +0400

    Array Traversal

--- src/duSolvingProblems/module5/CountGirls.java
+++ src/duSolvingProblems/module5/CountGirls.java
@@ -1,19 +1,19 @@
-package duSolvingProblems.module5;
-
-import org.apache.commons.csv.CSVRecord;
-
-public class CountGirls {
-    int totalGirls = 0;
-
-for (
-    CSVRecord record : parser) {
-        String gender = record.get(1);
-        int count = Integer.parseInt(record.get(2));
-
-        if (gender.equals("F")) {
-            totalGirls += count;
-        }
-    }
-
-System.out.println(totalGirls);
-}
+//package duSolvingProblems.module5;
+//
+//import org.apache.commons.csv.CSVRecord;
+//
+//public class CountGirls {
+//    int totalGirls = 0;
+//
+//for (
+//    CSVRecord record : parser) {
+//        String gender = record.get(1);
+//        int count = Integer.parseInt(record.get(2));
+//
+//        if (gender.equals("F")) {
+//            totalGirls += count;
+//        }
+//    }
+//
+//System.out.println(totalGirls);
+//}
